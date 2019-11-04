def4599be97593e6fbeef8b60db09bfc97c71d34
Hambush2 <55706777+Hambush2@users.noreply.github.com>
2019-11-04 16:11:33 +0000

    Added Some Animations, Completed Zombie Turning, and Damage
    Added blood animations, finished Zombie Turning, and player can take damage.

---
+++ Zombie Game/Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject prefabToSpawn;
+    public float adjustmentAngle = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Spawn()
+    {
+        Vector3 rotationDeg = transform.eulerAngles;
+        rotationDeg.z += adjustmentAngle;
+
+        Quaternion rotationRad = Quaternion.Euler(rotationDeg);
+
+        Instantiate(prefabToSpawn, transform.position, rotationRad);
+    }
+}
--- Zombie Game/Assets/Scripts/ZombieTurning.cs
+++ Zombie Game/Assets/Scripts/ZombieTurning.cs
@@ -1,37 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
 
 public class ZombieTurning : MonoBehaviour
 {
-    
+    Rigidbody2D rigidbody2D;
+
+    public float smoothing = 5.0f;
+    float x = 0.0f;
+    float y = 0.0f;
+    public float adjustmentAngle = 270.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.D) == true)
+        if (Input.GetKey(KeyCode.D) == true)
+
         {
-
-
+            x = 1;
+            float rotZ = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
+            Quaternion newRotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, rotZ + adjustmentAngle));
+            transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * smoothing);
 
         }
-        else if (Input.GetKeyDown(KeyCode.A) == true)
+        else if (Input.GetKey(KeyCode.A) == true)
         {
-
+            x = -1;
+            float rotZ = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
+            Quaternion newRotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, rotZ + adjustmentAngle));
+            transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * smoothing);
         }
 
-        if (Input.GetKeyDown(KeyCode.W) == true)
+        if (Input.GetKey(KeyCode.D) == false)
         {
-
+            x = 0;
         }
-        else if(Input.GetKeyDown(KeyCode.S) == true)
+        if (Input.GetKey(KeyCode.A) == false)
         {
+            x = 0;
+        }
 
+        if (Input.GetKey(KeyCode.W) == true)
+        {
+            y = 1;
+            float rotZ = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
+            Quaternion newRotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, rotZ + adjustmentAngle));
+            transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * smoothing);
+        }
+        else if(Input.GetKey(KeyCode.S) == true)
+        {
+            y = -1;
+            float rotZ = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
+            Quaternion newRotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, rotZ + adjustmentAngle));
+            transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * smoothing);
+        }
+
+        if (Input.GetKey(KeyCode.W) == false)
+        {
+            y = 0;
+        }
+        if (Input.GetKey(KeyCode.S) == false)
+        {
+            y = 0;
         }
     }
 }
